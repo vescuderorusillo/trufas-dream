16f4285895fc08347e60825aa8da9f438069a0a7
Victor Escudero <victor.escudero@bestsecret.com>
2020-04-05 19:54:27 +0200

    Changed bird image by trufa's image

--- Bird.lua
+++ Bird.lua
@@ -3,7 +3,7 @@ Bird = Class{}
 local GRAVITY = 20
 
 function Bird:init()
-    self.image = love.graphics.newImage('images/bird.png')
+    self.image = love.graphics.newImage('images/trufa.png')
     self.x = VIRTUAL_WIDTH / 2 - 8
     self.y = VIRTUAL_HEIGHT / 2 - 8
 
